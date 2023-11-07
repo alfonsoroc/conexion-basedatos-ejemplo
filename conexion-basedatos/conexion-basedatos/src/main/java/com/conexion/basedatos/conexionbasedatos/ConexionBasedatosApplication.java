40f88ed0ea6039ae1e8550d8822f85433910466e
alfonsoroc <alfonsoroc789@gmail.com>
2023-11-07 10:28:49 -0700

    Inicializar proyecto
    Inicializar proyecto

---
+++ conexion-basedatos/conexion-basedatos/src/main/java/com/conexion/basedatos/conexionbasedatos/ConexionBasedatosApplication.java
@@ -0,0 +1,13 @@
+package com.conexion.basedatos.conexionbasedatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexionBasedatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexionBasedatosApplication.class, args);
+	}
+
+}
---
+++ conexion-basedatos/conexion-basedatos/src/test/java/com/conexion/basedatos/conexionbasedatos/ConexionBasedatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conexion.basedatos.conexionbasedatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexionBasedatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
